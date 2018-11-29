180f1b729c89fa6113de66b0a9545d74c0de28ee
Shin Changseon <skywing27@gmail.com>
2018-11-29 16:28:35 +0900

    add login feature

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import LoginPage from './pages/LoginPage';
+import UserProvider from './contexts/UserContext';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/product/:productId" component={ProductPage} />
-          <Route path="/login" component={LoginPage} />
-        </>
+        <UserProvider>
+          <>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/product/:productId" component={ProductPage} />
+            <Route path="/login" component={LoginPage} />
+          </>
+        </UserProvider>
       </BrowserRouter>
     );
   }
--- src/components/LoginFormView.js
+++ src/components/LoginFormView.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 
 export default class LoginFormView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleFieldChange = (e, name) => {
+    this.setState({
+      [name]: e.target.value,
+    });
+  };
+
   render() {
+    const { username, password } = this.state;
+    const { onLogin } = this.props;
     return (
       <div>
-        <input type="text" />
-        <input type="password" />
-        <button>로그인</button>
+        <input
+          value={this.state.username}
+          type="text"
+          onChange={e => this.handleFieldChange(e, 'username')}
+        />
+        <input
+          value={this.state.password}
+          type="password"
+          onChange={e => this.handleFieldChange(e, 'password')}
+        />
+        <button onClick={() => onLogin(username, password)}>로그인</button>
       </div>
     );
   }
