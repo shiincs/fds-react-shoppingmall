c3937086be074e94c552448b8a01f34b42fda336
Shin Changseon <skywing27@gmail.com>
2018-11-30 11:22:13 +0900

    HeaderView status reset by key

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,5 +1,15 @@
+import React from 'react';
 import HeaderView from '../components/HeaderView';
-import { withUser } from '../contexts/UserContext';
+import { withUser, UserConsumer } from '../contexts/UserContext';
 
 // PC를 HOC로 감싸서 CC로 사용하는 패턴이 많이 사용된다. (실무에서)
-export default withUser(HeaderView);
+// export default withUser(HeaderView);
+
+// 인덱스페이지에서 로그아웃 시에도 헤더를 유지하기 위해 키를 넘겨준다.
+export default function Header(props) {
+  return (
+    <UserConsumer>
+      {value => <HeaderView key={value.username} {...value} />}
+    </UserConsumer>
+  );
+}
